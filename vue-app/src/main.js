06a3bd08b40802b6006f12ecfe1164dc6ecb73d7
Talar1997 <32823145+Talar1997@users.noreply.github.com>
2020-11-06 00:49:24 +0100

    Update
    Implemented vuex

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
+import { createStore } from 'vuex'
 import App from './App.vue'
+
+const store = createStore()
+const app = createApp()
+
+app.use(store)
 
 createApp(App).mount('#app')
